5975e01664f32e799f59f08a0d6bcf804dda2798
ShariarArefinOfficial <shariararefin0@gmail.com>
2024-04-15 03:16:14 +0600

    enviorment variable setup for firebase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,14 +3,16 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
+console.log('env updated',import.meta.env.VITE_messagingSenderId)
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAB4quQiCg4g5R-EkeGJ315a2gN_2e7q7E",
-  authDomain: "real-residence-cea18.firebaseapp.com",
-  projectId: "real-residence-cea18",
-  storageBucket: "real-residence-cea18.appspot.com",
-  messagingSenderId: "287958682922",
-  appId: "1:287958682922:web:a124014f27d773144b6253"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
